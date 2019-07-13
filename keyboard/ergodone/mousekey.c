723c27980cddad1fbdf149f005bde0bb6864c3b8
279554959 <279554959@qq.com>
2019-07-13 14:55:01 +0800

    Step3
    将摇杆的电位信号与鼠标键联系起来，目前算法仍有问题，待修改

--- keyboard/ergodone/mousekey.c
+++ keyboard/ergodone/mousekey.c
@@ -24,6 +24,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "debug.h"
 #include "mousekey.h"
 #include <LUFA/Drivers/Peripheral/ADC.h>
+//#include "keymap_in_eeprom.h"
+#include <math.h>
 
 
 
@@ -54,9 +56,29 @@ uint8_t mk_time_to_max = MOUSEKEY_TIME_TO_MAX;
 uint8_t mk_wheel_max_speed = MOUSEKEY_WHEEL_MAX_SPEED;
 uint8_t mk_wheel_time_to_max = MOUSEKEY_WHEEL_TIME_TO_MAX;
 //No2 joystick position
-uint16_t adc_move_x = 0;
-uint16_t adc_move_y = 0;
+static uint16_t adc_move_x = 0;
+static uint16_t adc_move_y = 0;
+static bool set_ADC_center;
+static bool set_ADC_ranger;
+static bool adc_enable;
+static uint16_t adc_setting_time = 0;
+typedef struct{
+    uint16_t xL;
+    uint16_t xR;
+    uint16_t yL;
+    uint16_t yR;
+    uint16_t xO;
+    uint16_t yO;
+}adc_range;
+adc_range adc_min, adc_max;
+adc_range *adc_range_p;
 
+typedef struct{
+    int16_t x;
+    int16_t y;
+    int16_t r;
+}adc_coordinate;
+adc_coordinate adc_now = { 0, 0, 0 }, adc_befor = { 0, 0, 0 };
 
 static uint16_t last_timer = 0;
 
@@ -99,44 +121,84 @@ static uint8_t wheel_unit(void)
     return (unit > MOUSEKEY_WHEEL_MAX ? MOUSEKEY_WHEEL_MAX : (unit == 0 ? 1 : unit));
 }
 
+static uint16_t delay(adc_coordinate position){
+    if (position.r < 10) {
+        return 50;
+    } else if (position.r < 45) {
+        return 25;//(150 - ((position.r - 10) * 100 / 35));
+    } else {
+        return 25;
+    }
+}
+
 void mousekey_task(void)
 {
 
     adc_move_x = ADC_GetChannelReading(ADC_REFERENCE_AVCC | ADC_CHANNEL12);
     adc_move_y = ADC_GetChannelReading(ADC_REFERENCE_AVCC | ADC_CHANNEL13);
+
+    if (set_ADC_center || set_ADC_ranger) {
+        adc_range_p = ((set_ADC_center == true) ? &adc_min : &adc_max);
+        if (adc_range_p->xL > adc_move_x) adc_range_p->xL = adc_move_x;
+        if (adc_range_p->xR < adc_move_x) adc_range_p->xR = adc_move_x;
+        if (adc_range_p->yL > adc_move_y) adc_range_p->yL = adc_move_y;
+        if (adc_range_p->yR < adc_move_y) adc_range_p->yR = adc_move_y;
+        if (timer_elapsed(adc_setting_time) > 10000) {
+              mousekey_send();
+        }
+    }
     // mousekey_debug();
-    if (timer_elapsed(last_timer) < (mousekey_repeat ? mk_interval : mk_delay*10))
+    if (adc_enable == false) return;
+
+    adc_now.x = (adc_move_x - adc_max.xO) * 100 / (adc_max.xR - adc_max.xL);
+    adc_now.y = (adc_move_y - adc_max.yO) * 100 / (adc_max.yR - adc_max.yL);
+    adc_now.r = sqrt(pow((adc_now.x),2) + pow((adc_now.y),2));
+
+    if (timer_elapsed(last_timer) < delay(adc_now))
         return;
 
-    if (mouse_report.x == 0 && mouse_report.y == 0 && mouse_report.v == 0 && mouse_report.h == 0)
-        return;
+    if (adc_now.r < 6) {
+        if ((adc_now.x != adc_befor.x) || (adc_now.y != adc_befor.y)){
+            mouse_report.x = adc_now.x - adc_befor.x;
+            mouse_report.y = adc_now.y - adc_befor.y;
+            memcpy(&adc_befor, &adc_now, sizeof(adc_coordinate));
+        } else {
+            mouse_report.x = 0;
+            mouse_report.y = 0;
+        }
+    } else {
+        mouse_report.x = adc_now.x;
+        mouse_report.y = -adc_now.y;
+    }
+    // if (mouse_report.x == 0 && mouse_report.y == 0 && mouse_report.v == 0 && mouse_report.h == 0)
+    //     return;
+    //
+    // if (mousekey_repeat != UINT8_MAX)
+    //     mousekey_repeat++;
 
-    if (mousekey_repeat != UINT8_MAX)
-        mousekey_repeat++;
 
-
-    if (mouse_report.x > 0) mouse_report.x = move_unit();
-    if (mouse_report.x < 0) mouse_report.x = move_unit() * -1;
-    if (mouse_report.y > 0) mouse_report.y = move_unit();
-    if (mouse_report.y < 0) mouse_report.y = move_unit() * -1;
+    // if (mouse_report.x > 0) mouse_report.x = move_unit();
+    // if (mouse_report.x < 0) mouse_report.x = move_unit() * -1;
+    // if (mouse_report.y > 0) mouse_report.y = move_unit();
+    // if (mouse_report.y < 0) mouse_report.y = move_unit() * -1;
 
     /* diagonal move [1/sqrt(2) = 0.7] */
-    if (mouse_report.x && mouse_report.y) {
-        mouse_report.x *= 0.7;
-        mouse_report.y *= 0.7;
-    }
+    // if (mouse_report.x && mouse_report.y) {
+    //     mouse_report.x *= 0.7;
+    //     mouse_report.y *= 0.7;
+    // }
 
-    if (mouse_report.v > 0) mouse_report.v = wheel_unit();
-    if (mouse_report.v < 0) mouse_report.v = wheel_unit() * -1;
-    if (mouse_report.h > 0) mouse_report.h = wheel_unit();
-    if (mouse_report.h < 0) mouse_report.h = wheel_unit() * -1;
+    // if (mouse_report.v > 0) mouse_report.v = wheel_unit();
+    // if (mouse_report.v < 0) mouse_report.v = wheel_unit() * -1;
+    // if (mouse_report.h > 0) mouse_report.h = wheel_unit();
+    // if (mouse_report.h < 0) mouse_report.h = wheel_unit() * -1;
 
     mousekey_send();
 }
 
 void mousekey_on(uint8_t code)
 {
-    if      (code == KC_MS_UP)       mouse_report.y = move_unit() * -1;
+    /*if      (code == KC_MS_UP)       mouse_report.y = move_unit() * -1;
     else if (code == KC_MS_DOWN)     mouse_report.y = move_unit();
     else if (code == KC_MS_LEFT)     mouse_report.x = move_unit() * -1;
     else if (code == KC_MS_RIGHT)    mouse_report.x = move_unit();
@@ -144,7 +206,7 @@ void mousekey_on(uint8_t code)
     else if (code == KC_MS_WH_DOWN)  mouse_report.v = wheel_unit() * -1;
     else if (code == KC_MS_WH_LEFT)  mouse_report.h = wheel_unit() * -1;
     else if (code == KC_MS_WH_RIGHT) mouse_report.h = wheel_unit();
-    else if (code == KC_MS_BTN1)     mouse_report.buttons |= MOUSE_BTN1;
+    else*/ if (code == KC_MS_BTN1)     mouse_report.buttons |= MOUSE_BTN1;
     else if (code == KC_MS_BTN2)     mouse_report.buttons |= MOUSE_BTN2;
     else if (code == KC_MS_BTN3)     mouse_report.buttons |= MOUSE_BTN3;
     else if (code == KC_MS_BTN4)     mouse_report.buttons |= MOUSE_BTN4;
@@ -156,7 +218,7 @@ void mousekey_on(uint8_t code)
 
 void mousekey_off(uint8_t code)
 {
-    if      (code == KC_MS_UP       && mouse_report.y < 0) mouse_report.y = 0;
+    /*if      (code == KC_MS_UP       && mouse_report.y < 0) mouse_report.y = 0;
     else if (code == KC_MS_DOWN     && mouse_report.y > 0) mouse_report.y = 0;
     else if (code == KC_MS_LEFT     && mouse_report.x < 0) mouse_report.x = 0;
     else if (code == KC_MS_RIGHT    && mouse_report.x > 0) mouse_report.x = 0;
@@ -164,7 +226,7 @@ void mousekey_off(uint8_t code)
     else if (code == KC_MS_WH_DOWN  && mouse_report.v < 0) mouse_report.v = 0;
     else if (code == KC_MS_WH_LEFT  && mouse_report.h < 0) mouse_report.h = 0;
     else if (code == KC_MS_WH_RIGHT && mouse_report.h > 0) mouse_report.h = 0;
-    else if (code == KC_MS_BTN1) mouse_report.buttons &= ~MOUSE_BTN1;
+    else*/ if (code == KC_MS_BTN1) mouse_report.buttons &= ~MOUSE_BTN1;
     else if (code == KC_MS_BTN2) mouse_report.buttons &= ~MOUSE_BTN2;
     else if (code == KC_MS_BTN3) mouse_report.buttons &= ~MOUSE_BTN3;
     else if (code == KC_MS_BTN4) mouse_report.buttons &= ~MOUSE_BTN4;
@@ -173,8 +235,8 @@ void mousekey_off(uint8_t code)
     else if (code == KC_MS_ACCEL1) mousekey_accel &= ~(1<<1);
     else if (code == KC_MS_ACCEL2) mousekey_accel &= ~(1<<2);
 
-    if (mouse_report.x == 0 && mouse_report.y == 0 && mouse_report.v == 0 && mouse_report.h == 0)
-        mousekey_repeat = 0;
+    // if (mouse_report.x == 0 && mouse_report.y == 0 && mouse_report.v == 0 && mouse_report.h == 0)
+    //     mousekey_repeat = 0;
 }
 
 void mousekey_send(void)
@@ -187,21 +249,68 @@ void mousekey_send(void)
 void mousekey_clear(void)
 {
     mouse_report = (report_mouse_t){};
-    mousekey_repeat = 0;
+    // mousekey_repeat = 0;
     mousekey_accel = 0;
 }
 
 static void mousekey_debug(void)
 {
     if (!debug_mouse) return;
-    print("mousekey [btn|x y v h](rep/acl)(adc): [");
+    if ((mousekey_accel & (1<<1)) && (mouse_report.buttons & MOUSE_BTN1)) {
+        //set center position
+        print("mousekey adc set center\n");
+        adc_setting_time = timer_read();
+        set_ADC_ranger = false;
+        set_ADC_center = true;
+        adc_min.xL = 1024;
+        adc_min.xR = 0;
+        adc_min.yL = 1024;
+        adc_min.yR = 0;
+        return;
+    }
+    if ((mousekey_accel & (1<<1)) && (mouse_report.buttons & MOUSE_BTN2)) {
+        //set ranger
+        print("mousekey adc set ranger\n");
+        adc_setting_time = timer_read();
+        set_ADC_center = false;
+        set_ADC_ranger = true;
+        adc_max.xL = 1024;
+        adc_max.xR = 0;
+        adc_max.yL = 1024;
+        adc_max.yR = 0;
+        return;
+    }
+    if ((mousekey_accel & (1<<1)) && (mouse_report.buttons & MOUSE_BTN3)) {
+        //adc enable
+        print("mousekey adc enable\n");
+        adc_enable = true;
+        return;
+    }
+    if ((mousekey_accel & (1<<1)) && (mouse_report.buttons & MOUSE_BTN4)) {
+        //adc enable
+        print("mousekey adc disable\n");
+        adc_enable = false;
+        return;
+    }
+    if (set_ADC_center || set_ADC_ranger) {
+        if (timer_elapsed(adc_setting_time) > 10000) {
+            set_ADC_center = false;
+            set_ADC_ranger = false;
+            adc_range_p->xO = (adc_range_p->xL + adc_range_p->xR) / 2;
+            adc_range_p->yO = (adc_range_p->yL + adc_range_p->yR) / 2;
+            print("adc set over\n<");
+            print_decs(adc_range_p->xL); print(",");
+            print_decs(adc_range_p->xR); print("> <");
+            print_decs(adc_range_p->yL); print(",");
+            print_decs(adc_range_p->yR); print(">\n");
+        }
+    }
+    print("mousekey [btn|x y v h](rep/acl): [");
     phex(mouse_report.buttons); print("|");
     print_decs(mouse_report.x); print(" ");
     print_decs(mouse_report.y); print(" ");
     print_decs(mouse_report.v); print(" ");
     print_decs(mouse_report.h); print("](");
     print_dec(mousekey_repeat); print("/");
-    print_dec(mousekey_accel); print(")(");
-    print_dec(adc_move_x); print("/");
-    print_dec(adc_move_y); print(")\n");
+    print_dec(mousekey_accel); print(")\n");
 }
